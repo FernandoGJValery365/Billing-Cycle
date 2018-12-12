e3727ff4357ce21df472bc04d3a98fd7cb25c87d
Fernando G J Valery <fernandogjvalery365@gmail.com>
2018-12-12 20:58:29 -0200

    Finalização do template

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            CopyRight &copy; Fernando Aprendizado; 2018.
+            <a href="#" target="_blank" rel="noopener noreferrer">Link que era para dar em algum lugar, :-(</a>
+        </strong>
+    </footer>
+);   
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../common/template/dependencies';
+import SideBar from '../common/template/sideBar';
+import Header from '../common/template/header';
+import Footer from '../common/template/footer';
 
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header/>
+        <SideBar/>
+        <div className="content-wrapper">
+            <h1>O conteúdo vai aqui </h1>
+        </div>
+        <Footer/>
     </div>
 );
